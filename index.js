fb38185e86ae1f06b60ad9dfa5b8e114c370b6b3
Ken Lovely <kenl_mcse@comcast.net>
2019-09-15 14:37:42 -0400

    Working demo

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-var http = require('http');
+// var http = require('http');
 
-const PORT = 80;
+// const PORT = 8080;
 
-function requestHandler(req, res) {
-	res.end(`Hello ${process.platform}`);
-}
+// function requestHandler(req, res) {
+// 	res.end(`Hello ${process.platform}`);
+// }
 
-var server = http.createServer(requestHandler);
+// var server = http.createServer(requestHandler);
 
-server.listen(PORT, function(){
-	console.log(`${process.env.NODE_ENV} server listening on port: ${PORT}. CTRL-C to exit.`);
+// server.listen(PORT, function(){
+// 	console.log(`${process.env.NODE_ENV} server listening on port: ${PORT}. CTRL-C to exit.`);
+// });
+
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type', 'text/plain');
+// 	res.end('WE BE WORKING 2\n');
+// });
+// server.listen(port, hostname, () => {
+// 	console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello Ken, Welcome to Docker\n');
 });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
