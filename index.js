7ea4ef679517458e86822c4d1d1fe15144b54fd4
Ken Lovely <kenl_mcse@comcast.net>
2019-09-15 14:52:57 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// var http = require('http');
-
-// const PORT = 8080;
-
-// function requestHandler(req, res) {
-// 	res.end(`Hello ${process.platform}`);
-// }
-
-// var server = http.createServer(requestHandler);
-
-// server.listen(PORT, function(){
-// 	console.log(`${process.env.NODE_ENV} server listening on port: ${PORT}. CTRL-C to exit.`);
-// });
-
-// const http = require('http');
-// const hostname = '127.0.0.1';
-// const port = 3000;
-// const server = http.createServer((req, res) => {
-// 	res.statusCode = 200;
-// 	res.setHeader('Content-Type', 'text/plain');
-// 	res.end('WE BE WORKING 2\n');
-// });
-// server.listen(port, hostname, () => {
-// 	console.log(`Server running at http://${hostname}:${port}/`);
-// });
 
 'use strict';
 
